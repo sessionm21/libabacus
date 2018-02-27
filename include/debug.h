127a59ba50cde057000acd29327b45d0e77ad75d
Danila Fedorin <danila.fedorin@gmail.com>
2018-02-27 11:08:14 -0800

    Add comments to the debug header.

--- include/debug.h
+++ include/debug.h
@@ -4,7 +4,16 @@
 #include "tree.h"
 #include <stdio.h>
 
+/**
+ * Prints the given tree to the given file.
+ * @param print the tree to print.
+ * @param file the file to print to.
+ */
 void libab_debug_fprint_tree(libab_tree* print, FILE* file);
+/**
+ * Prints the given tree to the standard output.
+ * @param print the tree to print.
+ */
 void libab_debug_print_tree(libab_tree* print);
 
 #endif
