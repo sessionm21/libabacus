78d24de15232ed338f268b39268a2fc9723353e5
Danila Fedorin <danila.fedorin@gmail.com>
2018-04-24 18:28:31 -0700

    Free type after retrieving necessary information.

--- src/value.c
+++ src/value.c
@@ -9,9 +9,9 @@ void libab_value_init(libab_value* value, void* data, libab_ref* type) {
 void libab_value_free(libab_value* value) {
     void (*free_function)(void*);
     libab_parsetype* value_type;
-    libab_ref_free(&value->type);
     value_type = libab_ref_get(&value->type);
     free_function = value_type->data_u.base->free_function;
+    libab_ref_free(&value->type);
     if (free_function)
         free_function(value->data);
 }
